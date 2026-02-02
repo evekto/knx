b4ae20b1cf63116b89ddc4add7b538227423ca2f
evekto <36211178+evekto@users.noreply.github.com>
2026-02-02 15:19:43 +0100

    Use require only with  static paths
    This allows the library to be used in bundlers without friction.

--- src/dptlib/index.js
+++ src/dptlib/index.js
@@ -33,26 +33,34 @@ Unlimited string 8859_1            .                       DPT 24	    DPT 24
 List 3-byte value                  3 Byte                  DPT 232	  DPT 232	RGB[0,0,0]...[255,255,255]
 */
 
-const fs = require('fs');
-const path = require('path');
 const util = require('util');
 const log = require('log-driver').logger;
 
 const dpts = {};
-for (const entry of fs.readdirSync(__dirname)) {
-  const matches = entry.match(/(dpt.*)\.js/);
-  if (!matches) continue;
-  const dptid = matches[1].toUpperCase(); // DPT1..DPTxxx
-  const mod = require(__dirname + path.sep + entry);
-  if (
-    !mod.hasOwnProperty('basetype') ||
-    !mod.basetype.hasOwnProperty('bitlength')
-  )
-    throw 'incomplete ' + dptid + ', missing basetype and/or bitlength!';
-  mod.id = dptid;
-  dpts[dptid] = mod;
-  //log.trace('DPT library: loaded %s (%s)', dptid, dpts[dptid].basetype.desc);
-}
+dpts.DP1 = require('./dpt1.js');
+dpts.DP2 = require('./dpt2.js');
+dpts.DP3 = require('./dpt3.js');
+dpts.DP4 = require('./dpt4.js');
+dpts.DP5 = require('./dpt5.js');
+dpts.DP6 = require('./dpt6.js');
+dpts.DP7 = require('./dpt7.js');
+dpts.DP8 = require('./dpt8.js');
+dpts.DP9 = require('./dpt9.js');
+dpts.DP10 = require('./dpt10.js');
+dpts.DP11 = require('./dpt11.js');
+dpts.DP12 = require('./dpt12.js');
+dpts.DP13 = require('./dpt13.js');
+dpts.DP14 = require('./dpt14.js');
+dpts.DP15 = require('./dpt15.js');
+dpts.DP16 = require('./dpt16.js');
+dpts.DP17 = require('./dpt17.js');
+dpts.DP18 = require('./dpt18.js');
+dpts.DP19 = require('./dpt19.js');
+dpts.DP20 = require('./dpt20.js');
+dpts.DP21 = require('./dpt21.js');
+dpts.DP232 = require('./dpt232.js');
+dpts.DP237 = require('./dpt237.js');
+dpts.DP238 = require('./dpt238.js');
 
 // a generic DPT resolution function
 // DPTs might come in as 9/"9"/"9.001"/"DPT9.001"
